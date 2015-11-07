a6327063efd2536df4f447ee961853f5b8715887
apoorvnandan <apoorvnandan@gmail.com>
2015-11-08 01:11:58 +0530

    Added Autosave

--- v1/app.js
+++ v1/app.js
@@ -1,68 +1,100 @@
-<!--
+/* 
 * version 0.0.1
 * created by Apoorv Nandan
--->
-<!doctype html>
-<html ng-app="plunker" >
-<head>
-	<meta charset="utf-8">
-	<title>Sub-Todos</title>
-	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.js"></script>
-	<script src="app.js"></script>
-	<style type="text/css">
-		span.tab{
-			padding: 0px 20px; /* Or desired space*/	
-		}
-		#newtask{
-		   width: 100%;
-\	    }
-		button{
-			margin-bottom: 5px;
-			margin-top: 5px;
-		}
-		.btn{
-			color:grey; 
-		}
-		body{
-			padding-left: 100px;
-			padding-right: 100px;
-			padding-top: 50px;
-			color: grey;
-			font-family: ‘Times New Roman’, Times, serif;
-		}
-		.glyphicon{
-			color: grey;
-		}
-		#header{
-			width: 100%;
-			font-size: large;
-			font-family: ‘Times New Roman’, Times, serif;
-			text-align: center;
-		}
-		form{
-			padding-top: 50px;
-		}
-  	</style>
-</head>
-<body ng-controller="MainCtrl">
-	<div id='header'><h>Sub-Todos</h></div>	
-	<hr />
-	<form role="form">
-		<div class="form-group">
-			<input type="text" class="form-control" id="newtask" placeholder="Enter Task" >
-		</div>
-	</form>
-	<choice-tree ng-model="myTree"></choice-tree>
+*/ 
+var app = angular.module('plunker', []);
 
-<script type="text/javascript">
-var task = "";
-var assign = function(){
-  	task = document.getElementById("newtask").value;
+function Choice(name, children) {
+    this.name = name;
+    this.checked = false;
+    this.children = children || [];
 }
-var clearfield = function(){
-  	document.getElementById("newtask").value= "";
+
+var apparel = new Choice('Todos', []);
+var loadedData = JSON.parse(localStorage.getItem('user'));
+var dataLoaded = false;
+if(loadedData){
+    var myChoice = convertToChoice(loadedData);
+    dataLoaded = true;
 }
-</script>
-</body>
-</html>
+
+function convertToChoice(d){
+	var root = makeChoice(d[0]);
+	return root;
+}
+
+function makeChoice(d){
+	var ret = new Choice(d.name);
+	ret.checked = d.checked;
+	var i;
+	if(d.children)
+	for(i = 0; i < d.children.length; i++){
+		ret.children.push(makeChoice(d.children[i]));
+	}
+	return ret;
+}
+
+app.controller('MainCtrl', function($scope) {
+  $scope.name = 'World';
+  if(!dataLoaded) $scope.myTree = [apparel]; 
+  else $scope.myTree = [myChoice]; 
+  $scope.saveData = function(){
+      localStorage.setItem('user', JSON.stringify($scope.myTree));
+  };
+});
+
+app.directive('choiceTree', function() {
+      return {
+          template: '<ul><choice ng-repeat="choice in tree"></choice></ul>',
+          replace: true,
+          transclude: true,
+          restrict: 'E',
+          scope: {
+              tree: '=ngModel'
+          }
+      };
+});
+
+app.directive('choice', function($compile) {
+    return { 
+        restrict: 'E',
+        template: '<li>' +
+        '<span ng-click="choiceClicked(choice)">' +
+        '<input type="checkbox" ng-checked="choice.checked"> {{choice.name}}' +
+        '</span>' + '<span class="tab"></span>' +
+        '<button class="btn btn-default btn-sm" ng-click="add(choice)">Add</button> <button class="btn btn-default btn-sm" ng-click="clear(choice)">Clear</button>' +
+        '</li>',
+        link: function(scope, elm, attrs) {
+            scope.choiceClicked = function(choice) {
+                choice.checked = !choice.checked;
+                function checkChildren(c) {
+                    angular.forEach(c.children, function(c) {
+                        c.checked = choice.checked;
+                        checkChildren(c);
+                    });
+                }
+                checkChildren(choice);
+            };
+            scope.clear = function(choice) {
+                choice.children=[];
+                angular.element(document.getElementById('save')).scope().saveData();
+            };
+    
+            scope.add = function(choice) {
+                assign();
+                var newName = task;
+                clearfield();
+                choice.children.push({name: newName,checked: false, children: []});
+                angular.element(document.getElementById('save')).scope().saveData();
+            };
+    
+            function updateChildren(e){
+                if(e.children.length>0){
+                    var childChoice = $compile('<choice-tree ng-model="choice.children"></choice-tree>')(scope)
+                    e.append(childChoice);
+                }
+            }
+            updateChildren(elm);
+        }
+    };
+});
